5dfbaf5d304db6d9e2a99ba7bf386dd434583b9b
Maziar Layeghkar <maziar.layeghkar@icloud.com>
2021-03-05 12:39:37 +0100

    Add 2 override methods into our Pencil design

--- Udder3/ViewController.swift
+++ Udder3/ViewController.swift
@@ -13,6 +13,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func didReceiveMemoryWarning() {
+    
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+
+    
 
 
 }
